844b8024c44dc73da3e9f57fec900e4f48ef8bbf
Thiago Youssef <thiagodyoussef@gmail.com>
2021-12-07 12:39:02 -0300

    Add `switch_source` lead hook

--- lib/simmonsdreamcomfort.rb
+++ lib/simmonsdreamcomfort.rb
@@ -2,9 +2,25 @@
 
 require_relative "simmonsdreamcomfort/version"
 
+require "f1sales_custom/hooks"
+
 module Simmonsdreamcomfort
   class Error < StandardError; end
 
   class F1SalesCustom::Hooks::Lead
+    def self.switch_source(lead)
+      message = lead.message
+      source_name = lead.source ? lead.source.name : ''
+
+      if message.include?('av._ibirapuera,_2453._moema')
+        "#{source_name} - Moema Loja 1"
+      elsif message.include?('av._ibirapuera,_3000._moema')
+        "#{source_name} - Moema Loja 2"
+      elsif message.include?('av._corifeu_de_azevedo_marques,_549_-_butantã')
+        "#{source_name} - Corifeu"
+      else
+        lead.source.name
+      end
+    end
   end
 end
--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -1,6 +1,43 @@
 require 'ostruct'
 require 'byebug'
-require 'f1sales_custom/hooks'
 
 RSpec.describe F1SalesCustom::Hooks::Lead do
+  let(:source_name) { 'Facebook - Simmons Dream Comfort' }
+  let(:source) do
+    source = OpenStruct.new
+    source.name = source_name
+
+    source
+  end
+
+  let(:lead) do
+    lead = OpenStruct.new
+    lead.source = source
+
+    lead
+  end
+
+  context 'when message contains "av._ibirapuera,_2453._moema"' do
+    before { lead.message = 'escolha_a_loja_por_onde_quer_ser_atendido: av._ibirapuera,_2453._moema' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Simmons Dream Comfort - Moema Loja 1')
+    end
+  end
+
+  context 'when message contains "av._ibirapuera,_3000._moema"' do
+    before { lead.message = 'escolha_a_loja_por_onde_quer_ser_atendido: av._ibirapuera,_3000._moema' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Simmons Dream Comfort - Moema Loja 2')
+    end
+  end
+
+  context 'when message contains "av._ibirapuera,_2453._moema"' do
+    before { lead.message = 'escolha_a_loja_por_onde_quer_ser_atendido: av._corifeu_de_azevedo_marques,_549_-_butantã' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Simmons Dream Comfort - Corifeu')
+    end
+  end
 end
