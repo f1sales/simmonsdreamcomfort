fa02c1c901c331785cc2df738158d7ee6c129070
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-08-10 14:01:57 -0300

    Unify Moema stores

--- lib/simmonsdreamcomfort.rb
+++ lib/simmonsdreamcomfort.rb
@@ -12,10 +12,8 @@ module Simmonsdreamcomfort
       message = lead.message || ''
       source_name = lead.source ? lead.source.name : ''
 
-      if message['av._ibirapuera,_2453_-_moema'] || message['av._ibirapuera,_2.453_-_moema']
-        "#{source_name} - Moema Loja 1"
-      elsif message['av._ibirapuera,_3000_-_moema']
-        "#{source_name} - Moema Loja 2"
+      if message['av._ibirapuera,_2453_-_moema'] || message['av._ibirapuera,_2.453_-_moema'] || message['av._ibirapuera,_3000_-_moema']
+        "#{source_name} - Moema"
       elsif message['av._corifeu_de_azevedo_marques,_549_-_butantã']
         "#{source_name} - Corifeu"
       elsif message['av._braz_leme,_757_-_santana'] || message['Av. Braz Leme, 757 - Santana, São Paulo - SP']
