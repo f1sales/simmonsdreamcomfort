87d97d53518264a878887adcdbe0f72e3b1a86d6
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-01-05 15:05:57 -0300

    F1API-552 Create Sumaré filter

--- lib/simmonsdreamcomfort.rb
+++ lib/simmonsdreamcomfort.rb
@@ -18,11 +18,21 @@ module Simmonsdreamcomfort
 
         return "#{source_name} - Braz Leme" if braz_leme?
 
+        return "#{source_name} - Sumaré" if sumare?
+
         source_name
       end
 
       def moema?
         @message[moema_address[0]] || @message[moema_address[1]] || @message[moema_address[2]]
+      end
+
+      def moema_address
+        %w[
+          av._ibirapuera,_2453_-_moema
+          av._ibirapuera,_2.453_-_moema
+          av._ibirapuera,_3000_-_moema
+        ]
       end
 
       def corifeu?
@@ -33,12 +43,8 @@ module Simmonsdreamcomfort
         @message['av._braz_leme,_757_-_santana'] || @message['Av. Braz Leme, 757 - Santana, São Paulo - SP']
       end
 
-      def moema_address
-        %w[
-          av._ibirapuera,_2453_-_moema
-          av._ibirapuera,_2.453_-_moema
-          av._ibirapuera,_3000_-_moema
-        ]
+      def sumare?
+        @message['av_sumare']
       end
     end
   end
