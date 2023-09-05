1e8cfd79b690a3b3866a1fd7ae027d1e44373956
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-09-05 16:37:19 -0300

    F1API-882: Create SOURCE_PATTERNS_PRODUCT

--- lib/simmonsdreamcomfort.rb
+++ lib/simmonsdreamcomfort.rb
@@ -8,14 +8,24 @@ module Simmonsdreamcomfort
 
   class F1SalesCustom::Hooks::Lead
     class << self
-      SOURCE_PATTERNS = {
+      SOURCE_PATTERNS_MESSAGE = {
         'Moema 1' => ['avibirapuera2453'],
         'Moema 2' => ['avibirapuera3000'],
         'Moema 3' => ['avibirapuera3399'],
         'Corifeu' => ['avcorifeudeazevedomarques'],
         'Braz Leme' => ['avbrazleme757'],
         'Sumaré' => ['avsumare'],
-        'Morumbi' => ['avavenidamorumbi'],
+        'Morumbi' => ['avavenidamorumbi']
+      }.freeze
+
+      SOURCE_PATTERNS_PRODUCT = {
+        'Moema 1' => ['moema'],
+        'Corifeu' => ['corifeu'],
+        'Sumaré' => ['sumar'],
+        'Morumbi' => ['morumbi'],
+        'Ibirapuera' => ['ibirapuera'],
+        'Indianópolis' => ['indian'],
+        'Alphaville' => ['alphavi']
       }.freeze
 
       def switch_source(lead)
@@ -24,13 +34,8 @@ module Simmonsdreamcomfort
         detected_source_message = detect_source_message
         return "#{source_name} - #{detected_source_message}" if detected_source_message
 
-        return "#{source_name} - Moema 1" if moema_1?
-        return "#{source_name} - Corifeu" if corifeu?
-        return "#{source_name} - Sumaré" if sumare?
-        return "#{source_name} - Morumbi" if morumbi?
-        return "#{source_name} - Ibirapuera" if ibirapuera?
-        return "#{source_name} - Indianópolis" if indianopolis?
-        return "#{source_name} - Alphaville" if alphaville?
+        detected_source_product = detect_source_product
+        return "#{source_name} - #{detected_source_product}" if detected_source_product
 
         source_name
       end
@@ -38,10 +43,15 @@ module Simmonsdreamcomfort
       private
 
       def detect_source_message
-        SOURCE_PATTERNS.each do |source_name, patterns|
-          return source_name if patterns.any? do |pattern|
-            message[pattern]
-          end
+        SOURCE_PATTERNS_MESSAGE.each do |source_name, patterns|
+          return source_name if patterns.any? { |pattern| message[pattern] }
+        end
+        nil
+      end
+
+      def detect_source_product
+        SOURCE_PATTERNS_PRODUCT.each do |source_name, patterns|
+          return source_name if patterns.any? { |pattern| product_name[pattern] }
         end
         nil
       end
@@ -57,34 +67,6 @@ module Simmonsdreamcomfort
       def message
         message_down = @lead.message&.downcase || ''
         message_down.gsub(/[^a-zA-Z\d]/, '')
-      end
-
-      def moema_1?
-        product_name['moema']
-      end
-
-      def corifeu?
-        product_name['corifeu']
-      end
-
-      def sumare?
-        product_name['sumar']
-      end
-
-      def morumbi?
-        product_name['morumbi']
-      end
-
-      def ibirapuera?
-        product_name['ibirapuera']
-      end
-
-      def indianopolis?
-        product_name['indian']
-      end
-
-      def alphaville?
-        product_name['alphavi']
       end
     end
   end
